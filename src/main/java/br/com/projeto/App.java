9a2ab0ce61b988b233de0f6731d2868a8d889091
Giuliana Silva Bezerra - Dataprev <giuliana.bezerra@dataprev.gov.br>
2019-06-04 09:54:41 -0300

    first commit

---
+++ src/main/java/br/com/projeto/App.java
@@ -0,0 +1,15 @@
+package br.com.projeto;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    final static Logger logger = Logger.getLogger(App.class);
+    public static void main( String[] args )
+    {
+        logger.info( "Hello World!" );
+    }
+}
